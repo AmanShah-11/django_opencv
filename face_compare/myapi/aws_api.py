1111b0dc7e5d6d7ae64e3ff8052f91560cfb6354
Aman Shah <a232shah@uwaterloo.ca>
2021-01-10 20:43:24 -0500

    Added aws and opencv implementation

---
+++ face_compare/myapi/aws_api.py
@@ -0,0 +1,37 @@
+import boto3
+import os
+
+
+def compare_faces(sourceFile, targetFile):
+    client = boto3.client('rekognition')
+    message = ""
+
+    imageSource = open('images\\source\\{}'.format(sourceFile), 'rb')
+    imageTarget = open('images\\target\\{}'.format(targetFile), 'rb')
+
+    response = client.compare_faces(SimilarityThreshold=80,
+                                    SourceImage={'Bytes': imageSource.read()},
+                                    TargetImage={'Bytes': imageTarget.read()})
+
+    for faceMatch in response['FaceMatches']:
+        position = faceMatch['Face']['BoundingBox']
+        similarity = str(faceMatch['Similarity'])
+        message = ('The face at ' +
+              str(position['Left']) + ' ' +
+              str(position['Top']) +
+              ' matches with ' + similarity + '% confidence')
+
+    imageSource.close()
+    imageTarget.close()
+    return len(response['FaceMatches'], message)
+
+
+def main():
+    source_file = 'images\\source\\amanshah.png'
+    target_file = 'images\\target\\amanshah.png'
+    face_matches = compare_faces(source_file, target_file)
+    print("Face matches: " + str(face_matches))
+
+
+if __name__ == "__main__":
+    main()
---
+++ face_compare/myapi/video.py
@@ -0,0 +1,87 @@
+import time
+import boto3
+import random
+import string
+import cv2
+import numpy as np
+import pyautogui
+from os import listdir
+from os.path import isfile, join
+
+from .aws_api import
+
+
+class BeginnerVideoFacialDetection:
+
+    # This method is written in a way such that only one face can be detected at a time
+    def screenshot_video_camera2(self, faces):
+        source_files = [f for f in listdir('images/source/')]
+        print(source_files)
+        target_file = ""
+        new_source_file = ""
+        for source_file in source_files:
+            print(source_file)
+            if pyautogui.locateOnScreen("images/source/{}".format(source_file), confidence=0.2):
+                for x, y, w, h in faces:
+                    my_screenshot = pyautogui.screenshot(region=[x, y, x+w, y+h])
+                    # my_screenshot = pyautogui.hotkey('alt', 'printscreen')
+                    my_screenshot = cv2.cvtColor(np.array(my_screenshot), cv2.COLOR_RGB2BGR)
+                    target_file = "".join(random.choice(string.ascii_lowercase) for i in range(10)) + ".png"
+                    print(target_file)
+                    cv2.imwrite("images/target/{}".format(target_file), my_screenshot)
+                    new_source_file = source_file
+                    break
+            else:
+                print("No match???????")
+        return new_source_file, target_file
+
+    def load_video_camera(self):
+        # casc_path = sys.argv[0]
+        source_file, target_file = "", ""
+        face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+        video_capture = cv2.VideoCapture(0)
+        count = 0
+        while True:
+            # Capture frame-by-frame
+            ret, frame = video_capture.read()
+
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+            # current = time.time()
+            # delta =+ current - previous
+            # previous = current
+
+            faces = face_cascade.detectMultiScale(
+                gray,
+                scaleFactor=1.1,
+                minNeighbors=5,
+                minSize=(30, 30),
+                # flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+            )
+
+            # Draw a rectangle around the faces
+            for (x, y, w, h) in faces:
+                 cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
+
+            # Display the resulting frame
+            cv2.imshow('Video', frame)
+            # print(count)
+            # print(delta)
+            # time.sleep(10)
+            # source_file, target_file = self.screenshot_video_camera2(faces)
+            # if (source_file != ""):
+            #     break
+            if count > 9:
+                source_file, target_file = self.screenshot_video_camera2(faces)
+                break
+            count = count + 1
+            # if cv2.waitKey(1) & 0xFF == ord('s'):
+            #
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        # When everything is done, release the capture
+        video_capture.release()
+        cv2.destroyAllWindows()
+        return source_file, target_file
